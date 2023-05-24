d682bb26ac0d68b3129ac9f518dd7c846e6c3323
OhNooooooooo <109457389+OhNooooooooo@users.noreply.github.com>
2023-05-24 18:09:11 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(640,480);
+    canvas.position(320,250);
+}
+
+function draw(){
+
+}
+
+function filter_save(){
+    save('image.png');
+}
